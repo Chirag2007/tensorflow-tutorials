43f90c2726b70ef3627a81581f439049cc392f89
Prem Ganeshkumar <ganeprem@gmail.com>
2017-10-17 22:15:43 -0400

    Run as session

---
+++ core-tutorial/hello_world.py
@@ -0,0 +1,17 @@
+# Need this flag to prevent useless warnings on OSX
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+
+# Constant nodes
+# takes no inputs, and it outputs a value it stores internally
+node1 = tf.constant(3.0, dtype=tf.float32)
+node2 = tf.constant(4.0) # also tf.float32 implicitly
+
+# Just prints info about the nodes. Doesn't actually run them.
+print(node1, node2)
+
+# Here, we actually run the nodes in a session
+sess = tf.Session()
+print(sess.run([node1, node2]))
